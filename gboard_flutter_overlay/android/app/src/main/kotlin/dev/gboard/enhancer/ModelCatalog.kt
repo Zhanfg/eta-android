a62d7981331307f70196bb89f944e0997ecd0d3a
Axymorrsen <Axymosen@outlook.com>
2026-09-18 20:25:37 +0800

    feat: add Flutter/Kotlin 0.2 overlay

---
+++ gboard_flutter_overlay/android/app/src/main/kotlin/dev/gboard/enhancer/ModelCatalog.kt
@@ -0,0 +1,7 @@
+package dev.gboard.enhancer
+
+internal object ModelCatalog {
+    const val HANDWRITING_MANIFEST = "https://dl.google.com/handwriting/models/handwriting_release.superpack_manifest.20260206.json"
+    const val SPEECH_FULL_MANIFEST = "https://dl.google.com/android/voice/gboard/en_us/ondevice_recognizer/superpacks-manifest-20191115.json"
+    const val SPEECH_SMALL_MANIFEST = "https://dl.google.com/android/voice/gboard/terse/superpacks-manifest-20210519.json"
+}
